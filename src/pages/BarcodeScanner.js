2fb3cd7e62193fb577e52f5ef8a9cd7731bf864d
Ariel Narciso <arielnarciso@gmail.com>
2019-12-08 02:42:18 -0300

    Tratando leitura de QRCode

--- src/pages/BarcodeScanner.js
+++ src/pages/BarcodeScanner.js
@@ -1,51 +1,75 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Alert, Text, View, StyleSheet, Button } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default class BarcodeScanner extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    scanned: false,
-  };
+	state = {
+		hasCameraPermission: null,
+		scanned: false,
+	};
 
-  async componentDidMount() {
-    this.getPermissionsAsync();
-  }
+	async componentDidMount() {
+		this.getPermissionsAsync();
+	}
 
-  getPermissionsAsync = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  };
+	getPermissionsAsync = async () => {
+		const { status } = await Permissions.askAsync(Permissions.CAMERA);
+		this.setState({ hasCameraPermission: status === 'granted' });
+	};
 
-  render() {
-    const { hasCameraPermission, scanned } = this.state;
+	errorMessage = (handle) => {
+		Alert.alert(
+			'QR Code inválido!',
+			'A informação do QR Code não se adequa ao padrão definido',
+			[{text: 'Ok', onPress: handle}],
+			{cancelable: false});
+	}
 
-    if (hasCameraPermission === null) {
-      return <Text>Requesting for camera permission</Text>;
-    }
-    if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    }
-    
-    return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-        }}>
-        <BarCodeScanner
-          onBarCodeScanned={this.handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
-        />
-      </View>
-    );
-  }
+	render() {
+		const { hasCameraPermission, scanned } = this.state;
 
-  handleBarCodeScanned = ({ type, data }) => {
-    this.setState({ scanned: true });
-    this.props.navigation.navigate('ScannerResponse', JSON.parse(data));
-    //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
+		if (hasCameraPermission === null) {
+			return <Text>Requesting for camera permission</Text>;
+		}
+		if (hasCameraPermission === false) {
+			return <Text>No access to camera</Text>;
+		}
+		
+		return (
+			<View
+				style={{
+					flex: 1,
+					flexDirection: 'column',
+					justifyContent: 'flex-end',
+				}}>
+				<BarCodeScanner
+					onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+					style={StyleSheet.absoluteFillObject}
+				/>
+			</View>
+		);
+	}
+
+	handleBarCodeScanned = ({ type, data }) => {
+
+		try {
+			const resposta = JSON.parse(data);
+			if (!resposta.hasOwnProperty('pergunta') ||
+					!resposta.hasOwnProperty('resposta') ||
+					!resposta.hasOwnProperty('alternativa') ||
+					!resposta.hasOwnProperty('dificuldade') ||
+					!resposta.hasOwnProperty('link')) {
+				this.setState({ scanned: true });
+				this.errorMessage(() => this.setState({scanned: false}));
+			} else {
+				//this.setState({ scanned: true });
+				this.props.navigation.navigate('ScannerResponse', resposta); // tem que resolver
+				//this.setState({scanned: false});
+			}
+		} catch (error) {
+				this.setState({ scanned: true });
+				this.errorMessage(() => this.setState({scanned: false}));
+		}
+	};
 }
