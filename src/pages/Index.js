dba84c3fb618622fd7e4a3622cdfbce984bdd429
Ariel Narciso <arielnarciso@gmail.com>
2019-12-08 22:27:30 -0300

    Estilizando botão

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, Button, View } from 'react-native';
+import { StyleSheet, TouchableHighlight, Text, View } from 'react-native';
 
 export default class Index extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Button title="Ler QRCODE" onPress={() => this.props.navigation.navigate('BarcodeScanner')} />
+				<TouchableHighlight
+					style={styles.button}
+					underlayColor='#00198c'
+					onPress={() => this.props.navigation.navigate('BarcodeScanner')}
+				>
+					<Text style={styles.text}>Ler QR Code</Text>
+				</TouchableHighlight>
 			</View>
 		);
 	}
@@ -14,8 +20,20 @@ export default class Index extends React.Component {
 const styles = StyleSheet.create({
   container: {
 		flex: 1,
-		backgroundColor: 'skyblue',
+		backgroundColor: '#d6d6d6',
 		alignItems: 'center',
 		justifyContent: 'center',
-  },
+	},
+	button: {
+		backgroundColor: '#052fe6',
+		borderRadius: 30,
+		borderColor: 'transparent'
+	},
+	text: {
+		fontSize: 20,
+		paddingVertical: 10,
+		paddingHorizontal: 40,
+		fontWeight: 'bold',
+		color: 'white'
+	}
 });
