8e15684633a92b5190b7dd2a7da125dca23ac5d9
Ariel Narciso <arielnarciso@gmail.com>
2019-12-08 22:25:52 -0300

    Adequando Header para telas diferentes.

--- App.js
+++ App.js
@@ -9,22 +9,57 @@ const StackNavigator = createStackNavigator({
     screen: Index
   },
   'BarcodeScanner': {
-    screen: BarcodeScanner
+    screen: BarcodeScanner,
+    navigationOptions: {
+      title: 'Leitura de QR Code',
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: 'bold'
+      }
+    }
   },
-  'ScannerResponse': {
-    screen: ScannerResponse
-  }
+  /*'ScannerResponse': {
+    screen: ScannerResponse,
+    navigationOptions: ({navigation}) => {
+      let {dificuldade} = navigation.state.params;
+      let backgroundColor, color;
+      if (dificuldade == 2) {
+        backgroundColor = 'yellow',
+        color = 'black';
+      } else {
+        color = 'white';
+        if (dificuldade == 1) {
+          backgroundColor = 'blue';
+        } else {
+          backgroundColor = 'red';
+        }
+      }
+      return ({
+        title: navigation.state.params.alternativa,
+        headerTintColor: color,
+        headerStyle: {
+          backgroundColor: backgroundColor,
+          borderBottomWidth: 2,
+          borderBottomColor: color
+        },
+        headerTitleStyle: {
+          fontSize: 25,
+          color: color
+        }
+      })
+    }
+  }*/
 }, {
   defaultNavigationOptions: {
     title: 'Eco Exploração',
     headerStyle: {
-      borderBottomWidth: 1,
-      borderBottomColor: 'blue'
+      backgroundColor: '#d6d6d6',
     },
     headerTitleStyle: {
       flexGrow: 1,
       textAlign: 'center',
-      fontSize: 25
+      fontSize: 25,
+      fontWeight: 'bold'
     }
   }
 });
